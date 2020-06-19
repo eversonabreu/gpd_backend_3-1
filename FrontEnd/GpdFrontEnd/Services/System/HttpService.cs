4a85a7baf1959de416fc8f2d013947a252d7e4d8
everson <everson.ean@gmail.com>
2020-06-19 09:38:54 -0300

    usuarios-fronend

--- FrontEnd/GpdFrontEnd/Services/System/HttpService.cs
+++ FrontEnd/GpdFrontEnd/Services/System/HttpService.cs
@@ -1,139 +1,41 @@
 ﻿using GpdFrontEnd.Infraestructure;
-using Microsoft.AspNetCore.Components;
-using System;
+using System.Collections.Generic;
 using System.Net.Http;
-using System.Text.Json;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace GpdFrontEnd.Services.System
 {
-    public class HttpService : ComponentBase
+    public class HttpService
     {
         private readonly HttpClient httpClient;
         private readonly Session session;
-        private readonly string controllerName;
 
-        public HttpService(HttpClient httpClient, Session session, string controllerName)
+        public HttpService(HttpClient httpClient, Session session)
         {
             this.httpClient = httpClient;
             this.session = session;
-            this.controllerName = controllerName;
         }
 
         public async Task<HttpClient> GetHttpAsync()
         {
+            httpClient.DefaultRequestHeaders.Clear();
             string token = await session.GetTokenAsync();
-            var http = new HttpClient();
-            http.DefaultRequestHeaders.Clear();
-            http.DefaultRequestHeaders.Add("authorization", $"Bearer {token}");
-            http.BaseAddress = new Uri($"{httpClient.BaseAddress.AbsoluteUri}{controllerName}");
-            return http;
+
+            if (token is null)
+            {
+                return null;
+            }
+
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+            return httpClient;
         }
 
-        public async Task GetAsync<TResult>(Action<TResult> success, Action<Exception> error, Action complete = null)
+        public async Task<ResultSet<TResult>> GetResultSetAsync<TResult>(HttpResponseMessage response, bool propertyNameCaseInsensitive = true)
         {
-            await GetAsync<TResult>(string.Empty, success, error, complete);
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<ResultSet<TResult>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = propertyNameCaseInsensitive });
+            return result;
         }
-
-        public async Task GetAsync<TResult>(string uri, Action<TResult> success, Action<Exception> error, Action complete = null)
-        {
-            TResult result = default;
-            Exception errorRequest = null;
-            Exception errorSuccess = null;
-            HttpResponseMessage httpResponseMessage = null;
-
-            using (var http = await GetHttpAsync())
-            {
-                try
-                {
-                    httpResponseMessage = await http.GetAsync(uri);
-                    if (httpResponseMessage.IsSuccessStatusCode)
-                    {
-                        string resultValue = await httpResponseMessage.Content.ReadAsStringAsync();
-                        if (typeof(TResult) == typeof(string))
-                        {
-                            result = (TResult)Convert.ChangeType(resultValue, typeof(TResult));
-                        }
-                        else
-                        {
-                            result = JsonSerializer.Deserialize<TResult>(resultValue, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-                        }
-                    }
-                    else
-                    {
-                        string message = await httpResponseMessage.Content.ReadAsStringAsync();
-                        throw new Exception(message);
-                    }
-                }
-                catch (Exception exc)
-                {
-                    string message = exc.Message;
-                    if (httpResponseMessage != null)
-                    {
-                        string reasonPhrase = httpResponseMessage.ReasonPhrase;
-                        int statusCode = (int)httpResponseMessage.StatusCode;
-                        errorRequest = new Exception($"ReasonPhrase: '{reasonPhrase}'.  StatusCode: '{statusCode}'. Message: '{message}'.");
-                    }
-                    else
-                    {
-                        errorRequest = new Exception($"Message: '{message}'.");
-                    }
-                }
-            }
-
-            if (errorRequest is null)
-            {
-                try
-                {
-                    success(result);
-                }
-                catch (Exception exSuccess)
-                {
-                    errorSuccess = exSuccess;
-                }
-            }
-            else
-            {
-                try
-                {
-                    error(errorRequest);
-                    errorRequest = null;
-                }
-                catch (Exception excError)
-                {
-                    errorRequest = excError;
-                }
-            }
-
-            try
-            {
-                complete?.Invoke();
-            }
-            finally
-            {
-                StateHasChanged();
-            }
-
-            if (errorSuccess != null)
-            {
-                RenderAfterError();
-                throw errorSuccess;
-            }
-            else if (errorRequest != null)
-            {
-                RenderAfterError();
-                throw errorRequest;
-            }
-        }
-
-        private void RenderAfterError()
-        {
-            Task.Run(async () =>
-            {
-                await Task.Delay(3000);
-                StateHasChanged();
-            });
-        }
-
     }
 }
